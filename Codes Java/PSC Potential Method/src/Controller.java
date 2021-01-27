f50cba10815d295ffc6e36c458b209637f809edf
LouisProffitX <louisproffit86@gmail.com>
2021-01-27 15:42:31 +0100

    Louis Potentiels

--- Codes Java/PSC Potential Method/src/Controller.java
+++ Codes Java/PSC Potential Method/src/Controller.java
@@ -8,31 +8,24 @@ public class Controller {
 	public static double tolerance = Math.pow(10, -7);
 	public static double width = 1;
 	public static double height = 1;
-	public static int n = 50;
-	public static int m = 50;
+	public static int n = 20;
+	public static int m = 20;
 	public static Grid grid;
 	public static Draw draw = new Draw();
 	public static LinkedList<Drone> drones = new LinkedList<Drone>();
-	public static LinkedList<Obstacle> obstacles = new LinkedList<Obstacle>();;
 	public static LinkedList<Zone> zones = new LinkedList<Zone>();
 	
 	public static void init() {
 		draw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		draw.setXscale(0, width);
 		draw.setYscale(0, height);
-		Drone drone1 = new Drone(0.9, 0.1, new Point(1, 1));
-		Obstacle obstacle1 = new Obstacle(new Point[] {new Point(0, 0), new Point(0.5, 0.5), new Point(0.3, 0.7)});
-		Obstacle obstacle2 = new Obstacle(new Point[] {new Point(0.8, 0.4), new Point(0.95, 0.9), new Point(0.6, 0.85), new Point(0.55, 0.53)});
+		Drone drone1 = new Drone(0.1, 0.1, new Point(0.1, 0.1));
 		Zone zone1 = new Zone(new Point[]{new Point(0.1, 0.8), new Point(0.2, 0.8), new Point(0.2, 0.85), new Point(0.15, 0.9)}, 1);
 		Zone zone2 = new Zone(new Point[]{new Point(0.8, 0.1), new Point(0.7, 0.2), new Point(0.95, 0.18)}, 1);
-		/*Zone zone2 = new Zone(new Point[]{new Point(0.3, 0.02), new Point(0.4, 0.32), new Point(0.6, 0.41), new Point(0.92, 0.28)}, 2);*/
 		drones.add(drone1);
-		obstacles.add(obstacle1);
-		obstacles.add(obstacle2);
 		zones.add(zone1);
 		zones.add(zone2);
-		/*zones.add(zone2);*/
-		grid = new Grid(width, height, n, m, obstacles, zones);
+		grid = new Grid(width, height, n, m, zones);
 		grid.draw(draw);
 	}
 	
@@ -44,13 +37,19 @@ public class Controller {
 		}
 		grid.updatePotential(drones);
 		grid.draw(draw);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		init();
-		/*for(int i = 0 ; i < 200 ; i++) {
+		for(int i = 0 ; i < 100 ; i++) {
 			update();
-		}*/
+		}
 	}
 }
 	
--- Codes Java/PSC Potential Method/src/Grid.java
+++ Codes Java/PSC Potential Method/src/Grid.java
@@ -11,104 +11,70 @@ public class Grid {
 	public double[][] potential; // Potentiel en cours d'action
 	public double minPotential;
 	public double maxPotential;
-	public double potentialGap;
-	public double totalPotential;
 	
-	public Grid(double width, double height, int n, int m, LinkedList<Obstacle> obstacles, LinkedList<Zone> zones){
+	public Grid(double width, double height, int n, int m, LinkedList<Zone> zones){
 		this.n = n;
 		this.m = m;
 		this.width = width;
 		this.height = height;
 		this.potential = new double[n + 1][m + 1];
 		this.map = new Map(n, m, width, height);
-		for (Obstacle obstacle : obstacles) this.map.addObstacle(obstacle);
 		for (Zone zone : zones) this.map.addInterestZone(zone);
 		double minPotentialIntermediate = map.priorityMap[0][0];
 		double maxPotentialIntermediate = map.priorityMap[0][0];
-		double totalPotentialIntermediate = 0;
 		double localPotential;
 		for (int i = 0 ; i <= n ; i++) {
 			for (int j = 0 ; j <= m ; j++) {
-				if (!map.obstacleMap[i][j]) {
-					localPotential = map.priorityMap[i][j];
-					this.potential[i][j] = localPotential;
-					if (localPotential < minPotentialIntermediate) minPotentialIntermediate = localPotential;
-					if (localPotential > maxPotentialIntermediate) maxPotentialIntermediate = localPotential;
-					totalPotentialIntermediate += localPotential;
-				}
+				localPotential = map.priorityMap[i][j];
+				this.potential[i][j] = localPotential;
+				if (localPotential < minPotentialIntermediate) minPotentialIntermediate = localPotential;
+				if (localPotential > maxPotentialIntermediate) maxPotentialIntermediate = localPotential;
 			}
 		}
-		this.totalPotential = totalPotentialIntermediate;
 		this.minPotential = minPotentialIntermediate;
 		this.maxPotential = maxPotentialIntermediate;
-		this.potentialGap = maxPotentialIntermediate - minPotentialIntermediate;
 	}
 	
 	public void updatePotential(LinkedList<Drone> drones) {
-		// Met à jour le potentiel :
-		// Augmente le potentiel dans le sillage des drones
-		double potentialCurrentVariation = 0;
 		double distanceSquare;
-		double increase;
+		
+		// Pour chaque drone , on modifie le potentiel exponentiellement autour de lui
 		for (Drone drone : drones) {
 			for (int i = 0 ; i <= n ; i++) {
 				for (int j = 0 ; j <= m ; j++) {
-					if (!map.obstacleMap[i][j]) { // Evite les obstacles
-						if ((i / n * width - drone.position.x) * drone.speedVector.x + (j / m * height - drone.position.y) * drone.speedVector.y < 0) { // Evite les points qui sont devant le drone
-							distanceSquare = Math.pow((float)(i) / n * width - drone.position.x, 2) + Math.pow((float)(j) / m * height - drone.position.y, 2);
-							if (distanceSquare > 0.071) { // Evite les points situés sur la ligne du drone (pour ne pas qu'il surfe
-								increase = drone.potentialIncreasePerStep * Math.exp(- distanceSquare / Math.pow(drone.decreasingSpaceInfluence, 2));
-								potentialCurrentVariation += increase;
-								potential[i][j] += increase;
-							}
-						}
-					}
+					distanceSquare = Math.pow((float)(i) / n * width - drone.position.x, 2) + Math.pow((float)(j) / m * height - drone.position.y, 2);
+					potential[i][j] += drone.decreasingSpaceInfluence / distanceSquare;
 				}
 			}
 		}
-		
-		// Le potentiel de la carte a été augmenté de potentialCurrentVariation
-		// Le potentiel total de la carte est map.totalPotential
 		// On diminue le potentiel de tous les points non obstacles proportionnelement à leur importance
 		for (int i = 0 ; i <= n ; i++) {
 			for (int j = 0 ; j <= m ; j++) {
-				if (!map.obstacleMap[i][j]) {
-					potential[i][j] -= (map.maximumPotential - map.priorityMap[i][j]) / totalPotential * potentialCurrentVariation;
-				} 
+				potential[i][j] -= map.priorityMap[i][j];
 			}
 		}
 		// Mise à jour des données sur le potentiel
 		double minPotentialIntermediate = map.priorityMap[0][0];
 		double maxPotentialIntermediate = map.priorityMap[0][0];
-		double totalPotentialIntermediate = 0;
 		double localPotential;
 		for (int i = 0 ; i <= n ; i++) {
 			for (int j = 0 ; j <= m ; j++) {
-				if (!map.obstacleMap[i][j]) {
-					localPotential = potential[i][j];
-					if (localPotential < minPotentialIntermediate) minPotentialIntermediate = localPotential;
-					if (localPotential > maxPotentialIntermediate) maxPotentialIntermediate = localPotential;
-					totalPotentialIntermediate += localPotential;
-				}
+				localPotential = potential[i][j];
+				if (localPotential < minPotentialIntermediate) minPotentialIntermediate = localPotential;
+				if (localPotential > maxPotentialIntermediate) maxPotentialIntermediate = localPotential;
 			}
 		}
-		this.totalPotential = totalPotentialIntermediate;
 		this.minPotential = minPotentialIntermediate;
 		this.maxPotential = maxPotentialIntermediate;
-		this.potentialGap = maxPotentialIntermediate - minPotentialIntermediate;
-		System.out.println(this.totalPotential);
-		System.out.println(potentialCurrentVariation);
 	}
 	
 	public void draw(Draw draw) {
 		// Affiche la grille
 		for (int i = 0 ; i < n ; i++) {
 			for (int j = 0 ; j < m ; j++) {
-				if (map.obstacleMap[i][j] & map.obstacleMap[i + 1][j] & map.obstacleMap[i + 1][j + 1]) draw.setPenColor(Color.BLACK);
-				else draw.setPenColor(getColor(potential[i][j], potential[i + 1][j], potential[i + 1][j + 1]));
+				draw.setPenColor(getColor(potential[i][j], potential[i + 1][j], potential[i + 1][j + 1]));
 				draw.filledPolygon(new double[]{i * width / n, (i + 1) * width / n, (i + 1) * width / n}, new double[]{j * height / m, j * height / m, (j + 1) * height / m});
-				if (map.obstacleMap[i][j] & map.obstacleMap[i][j + 1] & map.obstacleMap[i + 1][j + 1]) draw.setPenColor(Color.BLACK);
-				else draw.setPenColor(getColor(potential[i][j], potential[i][j + 1], potential[i + 1][j + 1]));
+				draw.setPenColor(getColor(potential[i][j], potential[i][j + 1], potential[i + 1][j + 1]));
 				draw.filledPolygon(new double[]{i * width / n, i * width / n, (i + 1) * width / n}, new double[]{j * height / m, (j + 1) * height / m, (j + 1) * height / m});
 			}
 		}
@@ -116,10 +82,9 @@ public class Grid {
 	
 	public Color getColor(double potential1, double potential2, double potential3) {
 		// Renvoie une couleur proportionelle à la hauteur du triangle
+		double potentialGap = this.maxPotential - this.minPotential;
 		if (potentialGap == 0) return Color.GREEN;
 		double proportion = (potential1 + potential2 + potential3 - 3 * minPotential) / (3 * potentialGap);
-		if (proportion < 0) proportion = 0;
-		if (proportion > 1) proportion = 1;
 		return new Color((int)(255 *(1 - proportion)), (int)(255 * proportion), 0);
 	}
 	
