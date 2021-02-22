e1f7043b00e3664b7ac416654a2f758f60e7697f
LouisProffitX <louisproffit86@gmail.com>
2021-02-22 23:05:05 +0100

    Louis clustering

--- Codes Java/Clustering/src/Controller.java
+++ Codes Java/Clustering/src/Controller.java
@@ -1,7 +1,14 @@
 public class Controller {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+
+        int numberOfCheckpoints = 1000;
+        int numberOfClusters = 5;
+
+        Checkpoint[] checkpoints = ClusteringAlgorithm.generateRandomCheckpoints(numberOfCheckpoints);
+        ClusteringAlgorithm clusteringAlgorithm = new ClusteringTree(checkpoints, numberOfClusters);
+        Cluster[] clusters = clusteringAlgorithm.getClusters();
+        ClusteringAlgorithm.showResult(clusters);
     }
-    
+
 }
